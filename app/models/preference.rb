be7ef57ab345a1b9ce1060f25e5170b50f69b1a8
Raghu Betina <raghu@firstdraft.com>
2016-12-02 18:17:55 +0000

    Direct association between preferences and routes

--- app/models/preference.rb
+++ app/models/preference.rb
@@ -1,6 +1,9 @@
 class Preference < ApplicationRecord
   # Direct associations
 
+  has_many   :routes,
+             :dependent => :destroy
+
   has_many   :users,
              :dependent => :destroy
 
--- app/models/route.rb
+++ app/models/route.rb
@@ -1,6 +1,8 @@
 class Route < ApplicationRecord
   # Direct associations
 
+  belongs_to :preference
+
   # Indirect associations
 
   # Validations
